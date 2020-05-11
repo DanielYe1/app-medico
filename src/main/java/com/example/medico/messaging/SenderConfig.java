b53179cbc188b3703d6c7e8a4ff7e5a63671007a
Daniel Ye <daniel.ye@m4u.com.br>
2020-05-11 15:49:44 -0300

    corrigindo configuracao de envio

--- src/main/java/com/example/medico/messaging/SenderConfig.java
+++ src/main/java/com/example/medico/messaging/SenderConfig.java
@@ -10,31 +10,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class SenderConfig {
 
     @Value("${queue.order.name}")
     private String orderQueue;
-
-    public static void main(String[] args) {
-        SpringApplication.run(SenderConfig.class, args);
-    }
 
     @Bean
     public Queue queue() {
         return new Queue(orderQueue, true);
     }
 
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(jsonMessageConverter());
-        return template;
-    }
 }
